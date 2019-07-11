31e23199bdf49d519f895b153c03683e79731dae
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 17:58:13 -0700

    cleared up codes and result

--- calculator.py
+++ calculator.py
@@ -13,48 +13,21 @@ while True:
     userinput = input()
     tokens = userinput.split(" ")
     operator = tokens[0]
+    result = None
+    operators = {"+": add, "-": subtract, "*": multiply,"/": divide}
+
     if userinput == "q":
             print("You have decided to quit")
             break
 
-    num1 = int(tokens[1])
-    num2 = int(tokens[2])
+    elif operator:
+        num1 = float(tokens[1])
+        num2 = float(tokens[2])
 
-    result = 0
+        result = operators[operator](num1, num2) 
 
-    operators = {"+": add(num1,num2), "-": subtract(num1,num2)}
-    
-    for op, fun in operators:
-        print(op)
-        result = op(float(num1), float(num2))
-
-    # if operator == "+":
-    #     result = add(float(num1), float(num2))
-    # elif operator == "-":
-
-    #     result = subtract(float(num1), float(num2))
-
-    # elif operator == "-":
-    #     result = subtract(float(num1), float(num2))
-    
+        
     else:
         print("please provide two numbers") 
 
     print(result)
-        # multiply(float, float) → float
-        
-        # divide(float, float) → float
-        
-        # square(float) → float
-        
-        # cube(float) → float
-        
-        # power(float, float) → float
-        
-        # mod(float, float) → float
-        
-    # tokenize input
-    # if the first token is "q":
-    #     quit
-    # else:
-    #     decide which math function to call based on first token
